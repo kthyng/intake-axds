b8b29c5a3db0dd614075ede8b6075394eff3ea99
Kristen Thyng <kthyng@gmail.com>
2022-12-02 15:19:36 -0600

    updated tests and added one for search_for

--- tests/test_axds_cat.py
+++ tests/test_axds_cat.py
@@ -9,42 +9,55 @@ from test_utils import FakeResponseParams
 from intake_axds.axds_cat import AXDSCatalog
 
 
-class FakeResponseSearch(object):
+class FakeResponse(object):
     def __init__(self):
         pass
 
     def json(self):
-        res = {"results": [{"uuid": "test_uuid"}]}
+        res = {"results": [{"uuid": "test_uuid",
+                            "label": "test_label",
+                            "description": "Test description.",
+                            "type": "platform2",
+                            "start_date_time": '2019-03-15T02:58:51.000Z',
+                            "end_date_time": '2019-04-08T07:54:56.000Z',
+                            "source": {"meta": {"attributes": {"institution": "example institution",
+                                                              "geospatial_bounds": "POLYGON ((-156.25421 20.29439, -160.6308 21.64507, -161.15813 21.90021, -163.60744 23.30368, -163.83879 23.67031, -163.92656 23.83893, -162.37264 55.991, -148.04915 22.40486, -156.25421 20.29439))"},
+                                                "variables": {"lon": "lon", "time": "time"}},
+                                       "files": {"data.csv.gz": {"url": "fake.csv.gz"},
+                                                 "data.viz.parquet": {"url": "fake.viz.parquet"},
+                                                 "deployment.nc": {"url": "fake.nc"},
+                                                 }
+                                       },
+                            }]}
         return res
 
 
-class FakeResponseMeta(object):
-    def __init__(self):
-        pass
+# class FakeResponseMeta(object):
+#     def __init__(self):
+#         pass
 
-    def json(self):
-        res = [
-            {
-                "data": {
-                    "resources": {
-                        "files": {
-                            "data.csv.gz": {"url": "fake.csv.gz"},
-                            "deployment.nc": {"url": "fake.nc"},
-                        },
-                    }
-                }
-            }
-        ]
+#     def json(self):
+#         res = [
+#             {
+#                 "data": {
+#                     "resources": {
+#                         "files": {
+#                             "data.csv.gz": {"url": "fake.csv.gz"},
+#                             "deployment.nc": {"url": "fake.nc"},
+#                         },
+#                     }
+#                 }
+#             }
+#         ]
 
-        return res
+#         return res
 
 
 @mock.patch("requests.get")
 def test_axds_catalog_platform_dataframe(mock_requests):
     """Test basic catalog API: platform as dataframe."""
-
-    mock_requests.side_effect = [FakeResponseSearch(), FakeResponseMeta()]
-
+    
+    mock_requests.side_effect = [FakeResponse()]
     cat = AXDSCatalog(datatype="platform2", outtype="dataframe")
     assert list(cat) == ["test_uuid"]
     assert cat["test_uuid"].urlpath == "fake.csv.gz"
@@ -54,8 +67,7 @@ def test_axds_catalog_platform_dataframe(mock_requests):
 def test_axds_catalog_platform_xarray(mock_requests):
     """Test basic catalog API: platform as xarray"""
 
-    mock_requests.side_effect = [FakeResponseSearch(), FakeResponseMeta()]
-
+    mock_requests.side_effect = [FakeResponse()]
     cat = AXDSCatalog(datatype="platform2", outtype="xarray")
     assert list(cat) == ["test_uuid"]
     assert cat["test_uuid"].urlpath == "fake.nc"
@@ -65,7 +77,7 @@ def test_axds_catalog_platform_xarray(mock_requests):
 def test_axds_catalog_platform_search(mock_requests):
     """Test catalog with space/time search."""
 
-    mock_requests.side_effect = [FakeResponseSearch(), FakeResponseMeta()]
+    mock_requests.side_effect = [FakeResponse()]
 
     kw = {
         "min_lon": -180,
@@ -82,13 +94,32 @@ def test_axds_catalog_platform_search(mock_requests):
 
 
 @mock.patch("requests.get")
+def test_axds_catalog_platform_search_for(mock_requests):
+    """Test catalog with space/time search and search_for."""
+
+    mock_requests.side_effect = [FakeResponse()]
+
+    kw = {
+        "min_lon": -180,
+        "max_lon": -156,
+        "min_lat": 50,
+        "max_lat": 66,
+        "min_time": "2021-4-1",
+        "max_time": "2021-4-2",
+        "search_for": "whale"
+    }
+
+    cat = AXDSCatalog(datatype="platform2", outtype="dataframe", kwargs_search=kw)
+    assert "&query=whale" in cat.search_url
+
+
+@mock.patch("requests.get")
 def test_axds_catalog_platform_search_variable(mock_requests):
     """Test catalog with variable search."""
 
     mock_requests.side_effect = [
         FakeResponseParams(),
-        FakeResponseSearch(),
-        FakeResponseMeta(),
+        FakeResponse(),
     ]
 
     criteria = {
@@ -150,7 +181,7 @@ def test_module_with_dataframe():
 
 @mock.patch("requests.get")
 def test_verbose(mock_requests, capfd):
-    mock_requests.side_effect = [FakeResponseSearch(), FakeResponseMeta()]
+    mock_requests.side_effect = [FakeResponse()]
 
     AXDSCatalog(datatype="platform2", outtype="dataframe", verbose=True)
 
